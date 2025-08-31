3144bf15094eeeba14937e692fbead2756a91067
Paras-mourya <parasmourya288@gmail.com>
2025-08-31 11:23:02 +0530

    first message

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,8 +1,7 @@
 import jwt from "jsonwebtoken";
 import AppError from "../utils/error.utils.js";
 import User from "../models/user.js";
- // apna User model import karo
-
+ 
 const isLoggedIn = async (req, res, next) => {
   try {
     const token = req.cookies?.token;
@@ -15,13 +14,13 @@ const isLoggedIn = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log("DEBUG >> Decoded token:", decoded);
 
-    // ✅ Database se user fetch
+   
     const user = await User.findById(decoded.id).select("-password");
     if (!user) {
       return next(new AppError("User not found", 404));
     }
 
-    req.user = user; // ✅ ab req.user._id available hoga
+    req.user = user; 
     next();
   } catch (error) {
     return next(new AppError("Invalid or expired token", 401));
